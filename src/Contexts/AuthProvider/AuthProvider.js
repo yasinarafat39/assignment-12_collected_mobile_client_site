775cd98fe24c3f75395f68f22f42843f6e2d850e
yasinarafat39 <yasinarafat2557@gmail.com>
2022-11-25 15:34:27 +0600

    Google Sign in method implement

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
 import app from '../../Firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 
 
 
@@ -10,8 +10,8 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
 
-    
 
     // create new user
     const createUser = (email, password) => {
@@ -35,12 +35,12 @@ const AuthProvider = ({ children }) => {
 
     // user LogOut System
     const userLogOut = () => {
-        signOut(auth)
+        return signOut(auth);
     }
 
     // Google Login
-    const handleGoogleLogin = () => {
-
+    const googleLogin = () => {
+        return signInWithPopup(auth, googleProvider);
     }
 
 
@@ -51,7 +51,8 @@ const AuthProvider = ({ children }) => {
         updateUser,
         userSignIn,
         handleForgetPassword,
-        userLogOut
+        userLogOut,
+        googleLogin
     }
 
     useEffect(() => {
--- src/utilites/SocialLogin/SocialLogin.js
+++ src/utilites/SocialLogin/SocialLogin.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
 import { FaFacebook, FaGoogle } from 'react-icons/fa';
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 
 const SocialLogin = () => {
+    const { googleLogin } = useContext(AuthContext);
+
+
+    const handleGoogleLogin = () => {
+        googleLogin()
+            .then(userCredential => {
+                const user = userCredential.user;
+                console.log(user);
+                toast.success("Login Success")
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
     return (
         <div className="flex justify-center space-x-4">
-            <button aria-label="Log in with Google" className="p-3 rounded-sm">
+            <button onClick={handleGoogleLogin} aria-label="Log in with Google" className="p-3 rounded-sm">
                 <FaGoogle className='text-3xl '></FaGoogle>
             </button>
             <button aria-label="Log in with Facebook" className="p-3 rounded-sm">
