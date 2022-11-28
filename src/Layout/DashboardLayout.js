f6580fb2041d2c49a92e0455c3e5ff39fcc05788
yasinarafat39 <yasinarafat2557@gmail.com>
2022-11-28 11:03:15 +0600

    Navbar responsive problem sovled

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -7,7 +7,24 @@ const DashboardLayout = () => {
     return (
         <div className="max-w-[1440px] mx-auto">
             <Navbar></Navbar>
-            <Outlet></Outlet>
+
+            <div className="drawer drawer-mobile">
+                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+                    <Outlet></Outlet>
+                    <label htmlFor="dashboard-drawer" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     );
--- src/componets/Pages/Shared/Navbar/Navbar.js
+++ src/componets/Pages/Shared/Navbar/Navbar.js
@@ -32,13 +32,13 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 ">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             {menu}
                             <div className="navbar-end lg:hidden md:hidden">
                                 {
@@ -66,19 +66,26 @@ const Navbar = () => {
                         {menu}
                     </ul>
                 </div>
-                <div className="navbar-end hidden lg:block md:block">
-                    {
-                        user?.uid ?
-                            <div className='flex items-center justify-end'>
-                                <img title={user.displayName} src={user.photoURL} className="w-[60px] h-[60px] rounded-full mr-3" alt="user Profile" />
-                                <button onClick={handleUserLogOut} className="btn">Sign Out</button>
-                            </div>
-                            :
-                            <div className='flex items-center justify-end'>
-                                <Link to="/signin" className="btn mr-2">Sign In</Link>
-                                <Link to="/signup" className="btn">Sign Up</Link>
-                            </div>
-                    }
+
+                <div className="navbar-end ">
+                    <div className='hidden lg:block md:block'>
+                        {
+                            user?.uid ?
+                                <div className='flex items-center justify-end'>
+                                    <img title={user.displayName} src={user.photoURL} className="w-[60px] h-[60px] rounded-full mr-3" alt="user Profile" />
+                                    <button onClick={handleUserLogOut} className="btn">Sign Out</button>
+                                </div>
+                                :
+                                <div className='flex items-center justify-end'>
+                                    <Link to="/signin" className="btn mr-2">Sign In</Link>
+                                    <Link to="/signup" className="btn">Sign Up</Link>
+                                </div>
+                        }
+                    </div>
+
+                    <label tabIndex={2} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
                 </div>
             </div>
         </div>
