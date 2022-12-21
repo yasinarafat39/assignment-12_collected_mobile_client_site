fed49cef777fe13a400aa877231b983dee6c2d2c
yasinarafat39 <yasinarafat2557@gmail.com>
2022-12-21 19:42:55 +0600

    Delete Banner style file

--- src/componets/Pages/Home/Banner/Banner.js
+++ src/componets/Pages/Home/Banner/Banner.js
@@ -7,7 +7,7 @@ import banner2 from '../../../../assets/banner/3056065.jpg';
 import banner3 from '../../../../assets/banner/3003640.jpg';
 import banner4 from '../../../../assets/banner/3108307.jpg';
 
-import bannerCSS from './Banner.css';
+ 
 
 const Banner = () => {
     return (
